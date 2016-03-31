5c73a350fa200f131c18dc6904ad5faa48876555
Sempeterc <bostjangrmek@gmail.com>
2016-03-31 17:12:54 +0200

    Added files via upload

---
+++ casino.py
@@ -0,0 +1,8 @@
+x = raw_input("vpisi tvoje stevilo")
+y = "5"
+if x == y:
+    print ("stevilka je prava")
+elif x > y:
+    print ("iskana stevilka je manjsa")
+else:
+    print ("iskana stevilka je vecja")
